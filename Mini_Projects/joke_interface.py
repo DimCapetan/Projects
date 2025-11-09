00ef986baaa65c910091b9e95eeca64fa0f33946
Dim Capetanakis <dim.capet@gmail.com>
2025-11-09 08:07:50 +0000

    .

--- Mini_Projects/joke_interface.py
+++ Mini_Projects/joke_interface.py
@@ -1,21 +1,20 @@
 from pyjokes import get_joke
-from tkinter import *
+from tkinter import ttk
+import tkinter as tk
 
 
 def on_click():
     joke = get_joke()
     label.config(text = joke)
 
-root = Tk()
+root = tk.Tk()
 root.title('The Trickster')
 root.geometry('1000x500')
-# frm = Frame(root, padding = 100)
-# frm.grid()
-label = Label(root, text = 'Hey, do you want to hear a joke?', justify = 'center')
+label = ttk.Label(root, text = 'Hey, do you want to hear a joke?', justify = 'center')
 label.pack(pady = 10)
-btn1 = Button(root, text = chr(0x1F504), command = on_click)
+btn1 = ttk.Button(root, text = chr(0x1F504), command = on_click)
 btn1.pack(pady = 10)
-btn2 = Button(root, text = 'You Done?', command = root.destroy)
-btn2.pack(pady = 20)
+btn2 = ttk.Button(root, text = 'You Done?', command = root.destroy)
+btn2.pack(pady = 10)
 
 root.mainloop()
